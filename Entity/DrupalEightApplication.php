192023d3d176e77cede1fc3baee5e8359319f149
PMassoels <massoelspieter@gmail.com>
2017-12-07 09:07:32 +0100

    Drupal 8 entity added Cleanup old code

---
+++ Entity/DrupalEightApplication.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace DigipolisGent\Domainator9k\AppTypes\DrupalEightBundle\Entity;
+
+use DigipolisGent\Domainator9k\CoreBundle\Entity\AbstractApplication;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class DrupalEightApplication
+ * @package DigipolisGent\Domainator9k\AppTypes\DrupalEightBundle\Entity
+ *
+ * @ORM\Entity()
+ */
+class DrupalEightApplication extends AbstractApplication
+{
+
+
+    const TYPE = "DRUPAL_EIGHT";
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     * @Assert\NotBlank()
+     * @Assert\Length(min="3", max="255")
+     */
+    protected $installProfile;
+
+
+    public function getType()
+    {
+        return self::TYPE;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInstallProfile()
+    {
+        return $this->installProfile;
+    }
+
+    /**
+     * @param string $installProfile
+     */
+    public function setInstallProfile(string $installProfile)
+    {
+        $this->installProfile = $installProfile;
+    }
+
+}
