bf79a2369958493cb52380d82970cf34c42b3e27
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2018-06-22 16:32:36 +0200

    WEBDOM-390: Fix extension name.

--- DependencyInjection/DigipolisGentDomainator9kAppTypesDrupalEightExtension.php
+++ DependencyInjection/DigipolisGentDomainator9kAppTypesDrupalEightExtension.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  * @see http://symfony.com/doc/current/cookbook/bundles/extension.html
  * @codeCoverageIgnore
  */
-class DigipolisGentDomainator9kAppTypeDrupalEightExtension extends Extension
+class DigipolisGentDomainator9kAppTypesDrupalEightExtension extends Extension
 {
 
     /**
